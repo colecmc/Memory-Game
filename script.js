503560534df332764d4cf59535e006a0d8df11a7
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-06-18 09:56:32 -0700

    JS works for if two cards dont match their data attributes, then they flip back over after one second. If they do match, then they don't flip back over.

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 let cardContainer = document.querySelector(".card-container");
 
-let isTwoShowing = false;
 let pair = [];
 let highScore = 0;
+let firstCard;
+let secondCard;
 
 cardContainer.addEventListener("click", function (e) {
-  let card = e.target;
   let dataKey = e.target.dataset.key;
   let dataNick = e.target.dataset.nick;
-  console.log(dataNick);
 
   if (pair.length === 0) {
     pair.push(dataNick);
-    card.parentElement.classList.toggle("flip");
+    firstCard = e.target.parentElement;
+    firstCard.classList.toggle("flip");
   } else if (pair.length === 1) {
     pair.push(dataNick);
-    card.parentElement.classList.toggle("flip");
-  } else if (
+    secondCard = e.target.parentElement;
+    secondCard.classList.toggle("flip");
+  }
+
+  if (
     pair.length === 2 &&
     pair[0] !== pair[1] &&
     pair[0] !== "undefined" &&
     pair[1] !== "undefined"
   ) {
     let timer = setInterval(function () {
-      card.parentElement.classList.toggle("flip");
+      firstCard.classList.toggle("flip");
+      secondCard.classList.toggle("flip");
       clearInterval(timer);
     }, 1000);
     pair = [];
@@ -35,16 +39,6 @@ cardContainer.addEventListener("click", function (e) {
     pair = [];
     highScore++;
   }
-
-  // let card = $(`.card-front[data-key="${ dataKey }"]`);
-  // console.log(card);
-
-  // isTwoShowing = false;
-  //
-  // let timer = setInterval(function () {
-  //   card.parentElement.classList.toggle("flip");
-  //   clearInterval(timer);
-  // }, 1000);
 });
 
 ///////////////////////////////////////////////////////////////
