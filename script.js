74887ecd8badcc68952ee90eb2682f5a72563a82
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-06-18 16:26:56 -0700

    refactored Card Flip code.

--- script.js
+++ script.js
@@ -28,68 +28,17 @@ cardContainer.addEventListener("click", function (e) {
     return;
   }
 
-  /////////////////////////////////////////////////////////////////ASSIGNING THE SELECTED CARD TO HAVE A DATA-ATTRIBUTE FLIPED = "TRUE"
-  // console.log(e.target.parentElement.classList.contains("flip"));
-  // if (e.target.parentElement.getAttribute("data-fliped") === "true") {
-  //   console.log("this contained the class flip" + e.target.parentElement);
-  //   return;
-  // }
+  /////////////////////////////////////////////////////////////////CONDITIONAL TO ONLY ALLOW 2 CARDS TO BE SHOWING AT ONCE
 
   if (numFliped >= 2) {
-    console.log("numfliped is 2 or more");
     return;
   }
-  // console.log(card.parentElement.dataset.fliped);
-  card.parentElement.setAttribute("data-fliped", "true");
 
   /////////////////////////////////////////////////////////////////CONDITIONALS TO ONLY LET TWO BE SELECTED AT ONCE
 
-  // let isTrueCount = 0;
-  //
-  // for (var child of children) {
-  //   if (child.dataset.fliped === "true") {
-  //     isTrueCount++;
-  //   }
-  // }
-  //
-  // if (isTrueCount >= 3) {
-  //   for (var child of children) {
-  //     // child.dataset.fliped === "false";
-  //     child.setAttribute("data-fliped", "false");
-  //     // console.log(child.dataset);
-  //     // console.log("changing data-flip to false");
-  //   }
-  //   // console.log(children);
-  //   // console.log(isTrueCount);
-  //   return;
-  // }
+  card.parentElement.setAttribute("data-fliped", "true");
 
-  /////////////////////////////////////////////////////////////////
-
-  // for (var child of children) {
-  //   if (child.dataset.fliped === "true") {
-  //     count++;
-  //     console.log(count);
-  //   }
-  //
-  //   if (count > 2) {
-  //     for (var child of children) {
-  //       child.setAttribute("data-fliped", "false");
-  //       console.log(child.dataset);
-  //       console.log("changing data-flip to false");
-  //     }
-  //     count = 0;
-  //     return;
-  //   }
-  // }
-  // console.log(count);
-  // console.log(children);
-  // console.log("made it out of the loop");
-  // count = 0;
-
-  // if (card.parentElement.dataset.fliped === "true") {
-  //   console.log("is true");
-  // }
+  /////////////////////////////////////////////////////////////////CONDITIONALS TO PUSH DATA ATTRIBUTES TO AN ARRAY IF ZERO OR ONLY ONE CARD IS SHOWING
 
   if (pair.length === 0) {
     pair.push(dataNick);
@@ -103,6 +52,8 @@ cardContainer.addEventListener("click", function (e) {
     secondKey = e.target.dataset.key;
   }
 
+  /////////////////////////////////////////////////////////////////CONDITIONAL TO CHECK TO SEE IF THOSE TWO IMAGES SHOWING ARE NOT MATCHES, IF NOT, THEN RESET THE PAIR ARRAY
+
   if (
     pair.length === 2 &&
     pair[0] !== pair[1] &&
@@ -113,19 +64,17 @@ cardContainer.addEventListener("click", function (e) {
     let timer = setInterval(function () {
       firstCard.classList.toggle("flip");
       secondCard.classList.toggle("flip");
-
       for (var child of children) {
         child.setAttribute("data-fliped", "false");
-        // console.log(child.dataset);
-        // console.log("changing data-flip to false");
       }
-      // console.log(children);
       pair = [];
       numFliped = 0;
-
       clearInterval(timer);
     }, 1000);
-  } else if (
+  }
+
+  /////////////////////////////////////////////////////////////////CONDITIONAL TO PREVENT THE USER FROM DOUBLE CLICKING THE SAME IMAGE
+  else if (
     pair[0] === pair[1] &&
     pair[0] !== "undefined" &&
     pair[1] !== "undefined" &&
@@ -137,17 +86,16 @@ cardContainer.addEventListener("click", function (e) {
       clearInterval(timer);
     }, 1000);
     pair = [];
-  } else if (
+  }
+
+  /////////////////////////////////////////////////////////////////CONDITIONAL TO CHECK IF BOTH IMAGES MATCH AND ARE UNIQUE KEY VALUES
+  else if (
     pair[0] === pair[1] &&
     pair[0] !== "undefined" &&
     pair[1] !== "undefined"
   ) {
-    // console.log("winning: " + pair);
-    // console.log(firstKey);
-    // console.log(secondKey);
     pair = [];
     highScore++;
-    // console.log(highScore);
   }
 });
 
